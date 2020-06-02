ad0cd79787902ffc59a35b175b5779be22087797
cory marsh <cory@bitslip6.com>
2020-06-02 16:46:58 -0600

    add additional text output, allow assert_eqic to accept nulls

--- assertions.php
+++ assertions.php
@@ -1,29 +1,30 @@
 <?php declare(strict_types=1);
 namespace {
 
-    function assert_base_condition(callable $test_fn, $actual, $expected, string $message) {
+    function assert_base_condition(callable $test_fn, $actual, $expected, string $message, string $output = "") {
         TinyTest\count_assertion();
         if ($test_fn($actual, $expected) === false) {
         	TinyTest\count_assertion_fail();
+			if ($output !== "") { echo $output; }
             throw new TinyTest\TestError($message, $actual, $expected);
         }
         TinyTest\count_assertion_pass();
     }
 
-    function assert_true($condition, string $message) {
-        assert_base_condition(function($condition, $expected) { return $condition; }, $condition, true, $message);
+    function assert_true($condition, string $message, string $output = "") {
+        assert_base_condition(function($condition, $expected) { return $condition; }, $condition, true, $message, $output);
     }
 
-    function assert_false($condition, string $message) {
-        assert_base_condition(function($condition, $expected) { return !$condition; }, $condition, false, $message);
+    function assert_false($condition, string $message, string $output = "") {
+        assert_base_condition(function($condition, $expected) { return !$condition; }, $condition, false, $message, $output);
     }
 
-    function assert_eq($actual, $expected, string $message) {
-        assert_base_condition(function($actual, $expected) { return $actual === $expected; }, $actual, $expected, $message);
+    function assert_eq($actual, $expected, string $message, string $output = "") {
+        assert_base_condition(function($actual, $expected) { return $actual === $expected; }, $actual, $expected, $message, $output);
     }
 
     function assert_eqic($actual, $expected, string $message) {
-        assert_base_condition(function($actual, $expected) { return ($actual != null && strcasecmp($actual, $expected) === 0); }, $actual, $expected, $message);
+        assert_base_condition(function($actual, $expected) { return ($actual === $expected || ($actual != null && strcasecmp($actual, $expected) === 0)); }, $actual, $expected, $message);
     }
 
     function assert_neq($actual, $expected, string $message) {
